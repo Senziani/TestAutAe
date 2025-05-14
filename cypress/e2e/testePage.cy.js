d50b04d32a55ce09a3637062d34f9e4b02cda59b
Rafael Senzini <rafael_senziani@hotmail.com>
2025-05-14 19:20:27 -0300

    initial commit

---
+++ cypress/e2e/testePage.cy.js
@@ -0,0 +1,10 @@
+/// <reference types = "cypress"/>
+
+describe('Test Case 1: Register User', () => {
+  it('passes', () => {
+    cy.visit('https://www.automationexercise.com/')
+
+    
+
+  })
+})
